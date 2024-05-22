ae4107e3f2635e72f81f0634141e270c17fa1eb5
inestell <166044521+inestell@users.noreply.github.com>
2024-05-22 10:39:42 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "a", "my", "that"];
+let adjective = ["great", "sad", "fine", "smart", "big"];
+let noun = ["chair", "lasagna", "light", "window", "tree"];
+let extensions = [".com", ".net", ".org", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainGenerator(arr1, arr2, arr3, arr4) {
+  let domains = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      for (let k = 0; k < arr3.length; k++) {
+        for (let l = 0; l < arr4.length; l++) {
+          domains.push(arr1[i] + arr2[j] + arr3[k] + arr4[l]);
+        }
+      }
+    }
+  }
+  console.log(domains);
+}
+
+domainGenerator(pronoun, adjective, noun, extensions);
